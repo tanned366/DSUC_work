e233ecfbe46795abb844a5b0221026c96079e9b1
Tanishq Bhatnagar <tanishqbhatnagar366@gmail.com>
2025-01-23 19:10:36 +0530

    Now it will take user inpu

--- str_rev.cpp
+++ str_rev.cpp
@@ -4,12 +4,14 @@
 using namespace std;
 
 int main() {
-    string str = "hello";
+    string str;
+    cout<<"Enter a string: ";
+    cin>>str;
     int start = 0;
     int end = str.length() - 1;
     while(start < end) {
         swap(str[start++], str[end--]);
     }
-    cout<<str;
+    cout<<"Reverse is: "<<str;
     return 0;
 }
