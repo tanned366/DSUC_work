94bd523881007a7d739f13008d16fda9ff13a491
Tanishq Bhatnagar <169049907+tanned366@users.noreply.github.com>
2025-02-11 22:26:03 +0530

    Added Quick Sort

--- search.c
+++ search.c
@@ -75,7 +75,27 @@ void mergesort(int arr[], int temp[], int left, int right) {
     }
 }
 
+void quicksort(int arr[], int low, int high) {
+    if(low < high) {
+        int pivot = partition(arr, low, high);
+        quicksort(arr, low, pivot-1);
+        quicksort(arr, pivot+1, high);
+    }
+}
 
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for(int j=low; j<high; j++) {
+        if(arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i+1], &arr[high]);
+    return i+1;     // return the index of the pivot element or partitioning index
+}
 
 int main() {
     int arr[] = {10, 20, 30, 40, 50, 60, 70};
