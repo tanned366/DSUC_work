6758d174562bfa76c0a2ef885f02530b5a8c1dc3
Tanishq Bhatnagar <169049907+tanned366@users.noreply.github.com>
2025-02-12 16:43:48 +0530

    Added insertion sort

--- search.c
+++ search.c
@@ -39,6 +39,18 @@ void bubblesort(int arr[], int n) {
     }
 }
 
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+       int key = arr[i];
+       int j = i-1;
+       while (j >= 0 && arr[j] > key) {
+        arr[j + 1] = arr[j];
+        j = j - 1;
+    }
+    arr[j + 1] = key;
+    }
+}
+
 void selectionsort(int arr[], int n) {
     for(int i=0; i<n-1; i++) {
         int minIndex = i;
